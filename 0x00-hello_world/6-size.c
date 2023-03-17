e8af3aae7681190592249438aa53c1848fcf4c6c
Ahmed <ahmedkhadr0025@gmail.com>
2023-03-16 19:56:14 -0700

    FIX files

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,7 +2,6 @@
 
 /**
  * main - Entry point
- * 
  * Description: prints sze of variables by using sizeOf function.
  *
  * Return: always 0 (success)
@@ -10,10 +9,10 @@
 
 int main(void)
 {
-        printf("Size of a char: %u byte(s)\n", sizeOf(char);
-	printf("Size of a int: %u byte(s)\n", sizeOf(int);
-	printf("Size of a long int: %u byte(s)\n", sizeOf(long int);
-	printf("Size of a long long int: %u byte(s)\n", sizeOf(long long int);
-	printf("Size of a float: %u byte(s)\n", sizeOf(float);
-        return (0);
+printf("Size of a char: %u byte(s)\n", sizeOf(char);
+printf("Size of a int: %u byte(s)\n", sizeOf(int);
+printf("Size of a long int: %u byte(s)\n", sizeOf(long int);
+printf("Size of a long long int: %u byte(s)\n", sizeOf(long long int);
+printf("Size of a float: %u byte(s)\n", sizeOf(float);
+return (0);
 }
