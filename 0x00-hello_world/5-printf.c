789a9e551600e8682155a20ed15253aa2432f2dd
Ahmed <ahmedkhadr0025@gmail.com>
2023-03-16 19:40:19 -0700

    ADD files

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * 
+ * Description: a C program that prints with printf function.
+ *
+ * Return: always 0 (success)
+ */
+
+int main(void)
+{
+	print("with proper grammar, but the outcome is a piece of art\n");
+	return (0);
+}
+
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * 
+ * Description: prints sze of variables by using sizeOf function.
+ *
+ * Return: always 0 (success)
+ */
+
+int main(void)
+{
+        print("Size of a char: %u byte(s)\n", sizeOf(char);
+	print("Size of a int: %u byte(s)\n", sizeOf(int);
+	print("Size of a long int: %u byte(s)\n", sizeOf(long int);
+	print("Size of a long long int: %u byte(s)\n", sizeOf(long long int);
+	print("Size of a float: %u byte(s)\n", sizeOf(float);
+        return (0);
+}
